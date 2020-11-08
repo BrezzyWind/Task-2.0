b13ad97147a062d914b702df95912c169a8c92f7
Filipp Ivkin <zloivoin4@gmail.com>
2020-11-09 01:54:07 +0300

    Minor Fix

--- src/main/java/Zadanie/Main.java
+++ src/main/java/Zadanie/Main.java
@@ -3,6 +3,7 @@ package Zadanie;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 import java.io.File;
 
 public class Main {
@@ -33,6 +34,7 @@ public class Main {
         JAXBContext jaxbContext1 = null;
         JAXBContext jaxbContext2 = null;
         JAXBContext jaxbContext3 = null;
+
         try {
             File firstClassFile=new File("C://2/untitled/FirstClassXML.txt");
             jaxbContext1 = JAXBContext.newInstance(FirstClass.class);
@@ -41,6 +43,10 @@ public class Main {
             jaxbMarshaller1.marshal(initFirst, firstClassFile);
             jaxbMarshaller1.marshal(initFirst, System.out);
 
+            Unmarshaller unmarshalled1 = jaxbContext1.createUnmarshaller();
+            FirstClass unmarshal = (FirstClass) unmarshalled1.unmarshal(firstClassFile);
+            System.out.println("Десиреализация поля в классе 1 "+unmarshal.booleanVariable);
+
             File secondClassFile=new File("C://2/untitled/SecondClassXML.txt");
             jaxbContext2 = JAXBContext.newInstance(ThirdClass.class);
             Marshaller jaxbMarshaller2 = jaxbContext2.createMarshaller();
@@ -48,15 +54,24 @@ public class Main {
             jaxbMarshaller2.marshal(initSecond, secondClassFile);
             jaxbMarshaller2.marshal(initSecond, System.out);
 
+            Unmarshaller unmarshalled2 = jaxbContext2.createUnmarshaller();
+            SecondClass unmarshal2 = (SecondClass) unmarshalled2.unmarshal(secondClassFile);
+            System.out.println("Десиреализация поля в классе 2 "+unmarshal2.doubleVariable);
+
             File thirdClassFile=new File("C://2/untitled/ThirdClassXML.txt");
             jaxbContext3 = JAXBContext.newInstance(ThirdClass.class);
             Marshaller jaxbMarshaller3 = jaxbContext3.createMarshaller();
             jaxbMarshaller3.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
             jaxbMarshaller3.marshal(initThird, thirdClassFile);
             jaxbMarshaller3.marshal(initThird, System.out);
+
+            Unmarshaller unmarshalled3 = jaxbContext3.createUnmarshaller();
+            ThirdClass unmarshal3 = (ThirdClass) unmarshalled3.unmarshal(thirdClassFile);
+            System.out.println("Десиреализация поля в классе 3 "+unmarshal3.intVariableSecond);
         } catch (JAXBException e) {
             e.printStackTrace();
         }
+
 
 
     }
